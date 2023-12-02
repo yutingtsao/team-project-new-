22464001215725e09c673dfc3e45386efc645699
yutingtsao <tsao850924@gmail.com>
2023-12-02 11:59:18 +0800

    add js function

--- Login/login.js
+++ Login/login.js
@@ -1,5 +1,17 @@
 
+   // 按下按鈕可以打開小視窗
 $('button').click(function(){
     $('.mask').css('display','block')
 })
+// 按下x可以關閉小視窗
+$('.bi').click(function(){
+    $('.mask').css('display','none')
+})
+ // 按下外面空白處可以關閉小視窗
+$('.mask').click(function(){
+    $(this).removeAttr('style')
+})
 
+$('.login_page').click(function(e){
+    e.stopPropagation()
+})
--- Register/Register.js
+++ Register/Register.js
@@ -1,3 +1,16 @@
+   // 按下按鈕可以打開小視窗
 $('button').click(function(){
     $('.mask').css('display','block')
+})
+// 按下x可以關閉小視窗
+$('.bi').click(function(){
+    $('.mask').css('display','none')
+})
+ // 按下外面空白處可以關閉小視窗
+ $('.mask').click(function(){
+    $(this).removeAttr('style')
+})
+
+$('.register_page').click(function(e){
+    e.stopPropagation()
 })
