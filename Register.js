868b0c443748e501f643e5662833f2966cf6c3a4
yutingtsao <tsao850924@gmail.com>
2023-12-03 17:27:21 +0800

    add countdown in register page

--- Register.js
+++ Register.js
@@ -2,10 +2,29 @@
 $('button').click(function(){
     $('.mask').css('display','block')
 })
-var main_bt = document.querySelector('.main_bt');
-main_bt.addEventListener('click',function(){
-    window.history.back(); // 在这里指定您想要跳转的 URL
+$('.main_bt').click(function(){
+    $('.register_page').hide()
+    $('.mask1').show()
 })
+
+// 秒數倒數
+$('.main_bt').click(function() {
+    var countdownElement = $(".second_5");
+    var seconds = 5;
+    updateCountdown();
+    var countdownInterval = setInterval(function() {
+        seconds-=1;
+        updateCountdown();
+        if (seconds === 0) {
+            clearInterval(countdownInterval); 
+            window.location.href = './login.html';
+        }
+    }, 1000);
+    
+    function updateCountdown() {
+        countdownElement.text(seconds);
+    }
+});
 
 var second_bt = document.querySelector('.second_bt');
 second_bt.addEventListener('click',function(){
