5eba1ec0294dc22330c95b1d502c0186c726a3b9
yutingtsao <150649681+yutingtsao@users.noreply.github.com>
2023-12-05 14:40:10 +0800

    add my point

---
+++ my_points.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    // 加載標頭內容
+    $("#headerContainer").load("./header.html",'');
+
+    // 加載頁腳內容
+    $("#footerContainer").load("./footer.html");
+});
+
+// 要放商品列表
+$('.bt1').click(function(){
+    window.location.href = ''; 
+})
+
